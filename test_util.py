c52dbc7b42d8b9845367019ca5b236d3a58977b7
Girish Kumar <zkmrgirish@gmail.com>
2020-04-14 14:31:32 +0530

    unit testing for util.parse func

---
+++ test_util.py
@@ -0,0 +1,18 @@
+import itertools
+from util import parse
+
+def test_parse():
+    testcase = 'input/test.5'
+    n, s, u = parse(testcase)
+
+    assert n == 2, f'{testcase} [n] got {n} expected 2'
+    assert len(s) == 2, f'{testcase} [len(s)] got {len(s)} expected 2'
+
+    res_s = [['0', '1'], ['0', '1']]
+    assert s == res_s, f'{testcase} [strategy_profile] got {s}, expected {res_s}'
+
+    strategy_vectors = [['0', '0'], ['0', '1'], ['1', '0'], ['1', '1'], ['1', 'a']]
+    results = [[-2, 2], [5, -5], [3, -3], [0, 0], None]
+
+    for sv, res in zip(strategy_vectors, results):
+        assert u(sv) == res, f'{testcase} [utility_function] failed'
